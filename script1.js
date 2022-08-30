5677bf08f923e460160220840d5bac6fb63011d3
aseeladel1 <aseeladel100@gmail.com>
2022-08-31 02:29:32 +0300

    initial commit

---
+++ script1.js
@@ -0,0 +1,12 @@
+function pizzaOven(crustType, sauceType, cheeses, toppings){
+    var pizza={};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+var p1=pizzaOven("deep dish","traditional","mozarella",["pepperoni","sausage"]);
+console.log(p1);
+var p2=pizzaOven("hand tossed","marinara",["mozzarella","feta"],["mushrooms","olives","onions"]);
+console.log(p2);
